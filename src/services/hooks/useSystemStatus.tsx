35ac42bc98681f068724dace477c66f323eb8bf6
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2024-01-26 12:29:26 +0300

    Update useSystemStatus.tsx

--- src/services/hooks/useSystemStatus.tsx
+++ src/services/hooks/useSystemStatus.tsx
@@ -65,7 +65,7 @@ function useSystemStatus() {
 }
 
 async function logs(key: string): Promise<ServiceStatus> {
-    const response = await fetch(`https://raw.githubusercontent.com/PupiPr0/fettle/main/public/status/${key}_report.log`);
+    const response = await fetch(`https://status.wildmoney.pro/status/${key}_report.log`);
     const text = await response.text();
     const lines = text.split("\n");
     try {
